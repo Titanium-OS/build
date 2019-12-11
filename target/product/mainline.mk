ff0e3034800102faf444e517b04c2715b2d862e7
Nikesh001 <nikkataria74@gmail.com>
2019-12-11 17:29:26 +0000

    build: remove some packages we don't track

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -32,8 +32,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 endif
 
 PRODUCT_PACKAGES += \
-    PhotoTable \
-    WallpaperPicker \
+    PhotoTable
 
 ifeq ($(TITANIUM_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
